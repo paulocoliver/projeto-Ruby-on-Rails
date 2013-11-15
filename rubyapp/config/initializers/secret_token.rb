d42412f32cf2d08c825dc351ffc62b8a8681ad3c
ro-damasceno <ro.damascenoo@gmail.com>
2013-11-15 15:54:59 -0200

    Projeto criado

---
+++ rubyapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubyapp::Application.config.secret_token = '95101f02faae20866b511dd016635903c7fe8bd65f544bebbbee4595c93321e9ac3bf3869806a26878e256ce938702a42c46b9a4a78090519a17a989cf1c9daf'
